81b80b5b25de969e5233473db5cf60dd439ef145
Lucas Marques <cmarques.lucas@gmail.com>
2017-02-08 23:15:57 -0200

    Projeto inicial

---
+++ src/main/java/com/lucasmarques/playground/struts/TutorialAction.java
@@ -0,0 +1,13 @@
+package com.lucasmarques.playground.struts;
+
+/**
+ * Created by lucascmarques on 08/02/17.
+ */
+public class TutorialAction {
+
+    public String execute() {
+        System.out.println("success");
+        return "success";
+    }
+
+}
